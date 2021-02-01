58692279fcd038025879d9d7504e98bcdbfb20d4
Jonathan Baez <jbaez9910@gmail.com>
2021-02-01 14:34:57 -0500

    instalacion de paquetes y configuracion inicial

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const router = require('./network/routes');
+
+var app = express();
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({ extended: true }));
+
+ router(app);
+
+app.use('/app', express.static('public'));
+app.listen(3000);
+
+console.log('la aplicacion esta escuchando en el puerto 3000');
