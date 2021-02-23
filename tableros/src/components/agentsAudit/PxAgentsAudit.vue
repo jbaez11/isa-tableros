c40c36a3441c3d9b1aa002dbbedfdebdfed949e0
Jonathan Baez <jbaez9910@gmail.com>
2021-02-23 13:33:43 -0500

    tablero agentsaudit part3

--- tableros/src/components/agentsAudit/PxAgentsAudit.vue
+++ tableros/src/components/agentsAudit/PxAgentsAudit.vue
@@ -3,96 +3,120 @@ import api from '@/api';
   <div class="app">
     <v-app>
       <v-main>
-        <h2 class="text-center">AGENTS AUDIT</h2>
-        <!--calendar-->
-        <v-layout row>
-          <v-flex xs12 sm6 offset-sm3>
-            <h4>Fecha a auditar</h4>
-          </v-flex>
-        </v-layout>
-        <v-layout row class="mb-2">
-          <v-flex xs12 sm6 offset-sm3>
-            <v-date-picker v-model="date"></v-date-picker>
-            <!-- <p>{{date}}</p>-->
-            {{ submitDate }}
-          </v-flex>
-        </v-layout>
-        <!--ENDcalendar-->
+        <h2 class="text-center orange--text">AGENTS AUDIT</h2>
+        <!--calendar and numCall-->
+        <v-container class="">
+          <v-row>
+            <v-col style="justify-content: center; align-items: center;text-align: center;">
+              
+                <h2 class="orange--text">Fecha <span style="color:gray;">a</span>  auditar</h2>
+                  <v-date-picker color="orange accent-3 lighten-1" v-model="date"></v-date-picker>
+                  <!-- <p>{{date}}</p>-->
+                  <p v-show="false">{{ submitDate }}</p>
+              
+            </v-col>
+            <v-col style="justify-content: center; align-items: center;text-align: center;">
+                  <h3 class="orange--text">Cantidad <span style="color:gray;">Grabaciones</span></h3>
+                  <span class="text-h2 orange--text">{{ this.cantidadLlamadas }}</span>
+              
+            </v-col>
+          </v-row>
+        </v-container>
+       
+        
+        <!--ENDcalendar and numCall-->
 
         <!--<input type="date" name="" id="" />-->
-        <span>{{ this.cantidadLlamadas }} llamdas</span>
         
-        
+
         <!--<span>Checked names: {{ checkedNames | json }}</span>-->
 
         <!--TABLE Mostrar nombre y cantidad-->
-        <v-simple-table class="mt-5" v-show="auditAgents != false">
+        <v-simple-table class="mt-5" v-show="topByCategory != false" >
           <template>
             <thead>
-              <th>NOMBRE</th>
-              <th>LLAMADAS</th>
-              <th>INFALTABLE POSITIVE</th>
-              <th>INFALTABLE NEGATIVE</th>
-              <th>NO PERMITIDA POSITIVE</th>
-              <th>NO PERMITIDA POSITIVE</th>
-              <th>RECOMENDACION POSITIVE</th>
-              <th>RECOMENDACION POSITIVE</th>
-              
+              <tr class="orange accent-3">
+                <th class="white--text ">NOMBRE</th>
+                <th class="white--text ">GRABACIONES</th>
+                <th class="white--text ">INFALTABLE POSITIVE</th>
+                <th class="white--text ">INFALTABLE NEGATIVE</th>
+                <th class="white--text ">NO PERMITIDA POSITIVE</th>
+                <th class="white--text ">NO PERMITIDA NEGATIVE</th>
+                <th class="white--text ">RECOMENDACION POSITIVE</th>
+                <th class="white--text ">RECOMENDACION NEGATIVE</th>
+              </tr>
             </thead>
             <tbody>
-              <tr v-for="agentAudit in agentCategoryTotals" :key="agentAudit.name">
-                <td>{{ agentAudit.name}}</td>
-                <td>{{ agentAudit.results.keyfiles.length}}</td>
-                <td>{{ agentAudit.results.infaltable.positive || 0}}</td>
-                <td>{{ agentAudit.results.infaltable.negative || 0}}</td>
-                <td>{{ agentAudit.results["no permitida"].positive || 0}}</td>
-                <td>{{ agentAudit.results["no permitida"].negative || 0}}</td>
-                <td>{{ agentAudit.results["recomendación"].positive || 0}}</td>
-                <td>{{ agentAudit.results["recomendación"].negative || 0}}</td>
+               <tr  
+                v-for="agentAudit in topByCategory"
+                :key="agentAudit.name"
+              >
+                <td>{{agentAudit.name}}</td>
+                <td style="text-align: center;">{{agentAudit.results.records}}</td>
+                <td style="text-align: center;">{{agentAudit.results["infaltables positivas"]}}</td>
+                <td style="text-align: center;">{{agentAudit.results["infaltables negativas"]}}</td>
+                <td style="text-align: center;">{{agentAudit.results["no permitidas positivas"]}}</td>
+                <td style="text-align: center;">{{agentAudit.results["no permitidas negativas"]}}</td>
+                <td style="text-align: center;">{{agentAudit.results["recomendaciones positivas"]}}</td>
+                <td style="text-align: center;">{{agentAudit.results["recomendaciones negativas"]}}</td>
                 
-                
-              </tr>
+              </tr> 
             </tbody>
           </template>
         </v-simple-table>
-        
+
         <!--ENDTABLE Mostrar nombre y cantidad-->
         <!--TABLE LLAMADAS-->
-        <v-simple-table class="mt-5" v-show="auditAgents != false">
+        <v-simple-table class="mt-5" v-show="topByCategory != false">
           <template>
             <thead>
-              <th>LLAMADA</th>
-              <th>TOTAL</th>
-              
+              <tr class="orange accent-3">
+                <th class="white--text ">GRABACIÓN</th>
+                <th class="white--text ">INFALTABLE POSITIVE</th>
+                <th class="white--text ">INFALTABLE NEGATIVE</th>
+                <th class="white--text ">NO PERMITIDA POSITIVE</th>
+                <th class="white--text ">RECOMENDACION POSITIVE</th>
+              </tr>
             </thead>
             <tbody>
-              <tr v-for="agentAudit in agentCategoryTotals" :key="agentAudit.name">
-                <td>{{ agentAudit.results.keyfiles }}</td>
-                
-              </tr>
+               <tr
+                  v-for="agentAudit in recordsByCategory"
+                :key="agentAudit.name"
+              >
+                <td>{{agentAudit.name}}</td>
+                <td style="text-align: center;">{{agentAudit.results.infaltable.positive}}</td>
+                <td style="text-align: center;">{{agentAudit.results.infaltable.negative}}</td>
+                <td style="text-align: center;">{{agentAudit.results["no permitida"].positive}}</td>
+                <!-- <td>{{agentAudit.results["no permitida"].negative}}</td> -->
+                <td style="text-align: center;">{{agentAudit.results["recomendación"].positive}}</td>
+                <!-- <td>{{agentAudit.results["recomendación"].negative}}</td> -->
+              </tr> 
             </tbody>
           </template>
         </v-simple-table>
         <!--ENDTABLE LLAMADAS-->
-        <v-simple-table class="mt-5" v-show="auditAgents != false">
+        <v-simple-table class="mt-5" v-show="topByCategory != false" >
           <template>
             <thead>
-              <th>FROM</th>
-              <th>KEYWORD</th>
-              <th>TO</th>
-              <th>CONFIDENCE</th>
-              <th>MODULE</th>
-              <th>CATEGORY</th>
+              <tr class="orange accent-3">
+                <th class="white--text ">FROM</th>
+                <th class="white--text ">KEYWORD</th>
+                <th class="white--text ">TO</th>
+                <th class="white--text ">CATEGORY</th>
+                <th class="white--text ">MODULE</th>
+              </tr>
             </thead>
             <tbody>
-              <tr v-for="agentAudit in agentCategoryTotals" :key="agentAudit._id">
+              <tr
+                 
+              >
+               
                 <td>1</td>
                 <td>1</td>
                 <td>1</td>
                 <td>1</td>
                 <td>1</td>
-                <td>1</td>
-                <td>1</td>
+
                 <!-- <td>{{agentAudit.keywords}}</td> -->
               </tr>
             </tbody>
@@ -122,12 +146,12 @@ export default {
   data() {
     return {
       auditAgents: {},
-      date: "2021-02-21",
+      date: '2021-02-23',
       cantidadLlamadas: 0,
-      totalCategory:[],
-      agentCategoryTotals:[]
-      
-
+      totalCategory: [],
+      recordsByCategory: [],
+      topByCategory:[],
+      grabaciones:[]
     };
   },
   created() {
@@ -138,75 +162,115 @@ export default {
       const response = await this.axios.get(
         url + `?eventDate=${this.date}T00:00:00.000Z`
       );
-    //   console.log('toda la dta',response.data.body);
+      //   console.log('toda la dta',response.data.body);
       this.auditAgents = response.data.body;
-      console.log('this.auditAgents',this.auditAgents);
       this.cantidadLlamadas = this.auditAgents.length;
-       let agentCategoryTotals = {}; 
-      for(let i=0; i<this.auditAgents.length;i++){
-        //   console.log('auditAgetn', this.auditAgents[i].agent.name);
-          let nameAgent = this.auditAgents[i].agent.name;
-          if(!(nameAgent in agentCategoryTotals)){
-              agentCategoryTotals[nameAgent] = {};
-             (agentCategoryTotals[nameAgent])["keyfiles"]=[];
+
+
+      let recordsByCategory = {};
+      for (let i = 0; i < this.auditAgents.length; i++) {
+        let nameAgent = this.auditAgents[i].agent.name;
+        if (!(nameAgent in recordsByCategory)) {
+          recordsByCategory[nameAgent] = {};
+          recordsByCategory[nameAgent] = {};
+        }
+        let keyfile = this.auditAgents[i].keyfile;
+        (recordsByCategory[nameAgent])[keyfile]={};
+
+        for (let keyword in this.auditAgents[i].keywords) {
+          
+          let category = this.auditAgents[i].keywords[keyword].clasification.category;
+          if (!(category in  (recordsByCategory[nameAgent])[keyfile])) {
+             (recordsByCategory[nameAgent][keyfile])[category]={};
           }
-          let keyfile = this.auditAgents[i].keyfile;
-          //console.log('keyfile = ',keyfile)
-          ((agentCategoryTotals[nameAgent])["keyfiles"]).push(keyfile);
+          let haveResults = false;
+          if (this.auditAgents[i].keywords[keyword].results.length > 0) {
+            haveResults = true;
+          }
+
+          if (haveResults) {
+            if (!("positive" in recordsByCategory[nameAgent][keyfile][category])) {
+              recordsByCategory[nameAgent][keyfile][category]["positive"] = 1;
+            } else {
+              recordsByCategory[nameAgent][keyfile][category]["positive"] += 1;
+            }
+          } else {
+            if (!("negative" in recordsByCategory[nameAgent][keyfile][category])) {
+              recordsByCategory[nameAgent][keyfile][category]["negative"] = 1;
+            } else {
+              recordsByCategory[nameAgent][keyfile][category]["negative"] += 1;
+            }
+          }
+        }
+      }
+      let agentSelected="LUZ STELLA  PINILLA BEJARANO";
+      let recordsByCategoryArray=[];
+      for (let keyfile in recordsByCategory[agentSelected]) {
+        let infoByAgent = {
+          name: keyfile,
+          results: recordsByCategory[agentSelected][keyfile]
+        };
+
+        recordsByCategoryArray.push(infoByAgent);
+      }
+      this.recordsByCategory = recordsByCategoryArray;
+      console.log("this.recordsByCategory", this.recordsByCategory);
+      console.log("recordsByCategory", recordsByCategory);
+      let topByCategory = {};
+
+      for(let agent in recordsByCategory){
+        if(!(agent in topByCategory)){
+          topByCategory[agent]={};
+          (topByCategory[agent])["records"]=0;
+          (topByCategory[agent])["infaltables positivas"]=0;
+          (topByCategory[agent])["infaltables negativas"]=0;
+          (topByCategory[agent])["no permitidas positivas"]=0;
+          (topByCategory[agent])["no permitidas negativas"]=0;
+          (topByCategory[agent])["recomendaciones positivas"]=0;
+          (topByCategory[agent])["recomendaciones negativas"]=0;
+
+        }
+        for(let keyfile in recordsByCategory[agent]){
+          (topByCategory[agent])["records"] +=1;
+          console.log("key",keyfile)
+           if("positive" in (recordsByCategory[agent][keyfile])["infaltable"]){
+             (topByCategory[agent])["infaltables positivas"]+= recordsByCategory[agent][keyfile]["infaltable"]["positive"];
+           }
+           if("negative" in recordsByCategory[agent][keyfile]["infaltable"]){
+             (topByCategory[agent])["infaltables negativas"]+= recordsByCategory[agent][keyfile]["infaltable"]["negative"] ;
+           }
+           if("positive" in recordsByCategory[agent][keyfile]["no permitida"]){
+             (topByCategory[agent])["no permitidas positivas"]+=recordsByCategory[agent][keyfile]["no permitida"]["positive"] ;
+           }
+           if("negative" in recordsByCategory[agent][keyfile]["no permitida"]){
+             (topByCategory[agent])["no permitidas negativas"]+=recordsByCategory[agent][keyfile]["no permitida"]["negative"] ;
+           }
+           if("positive" in recordsByCategory[agent][keyfile]["recomendación"]){
+             (topByCategory[agent])["recomendaciones positivas"]+=recordsByCategory[agent][keyfile]["recomendación"]["positive"] ;
+           }
+           if("negative" in recordsByCategory[agent][keyfile]["recomendación"]){
+             (topByCategory[agent])["recomendaciones negativas"]+=recordsByCategory[agent][keyfile]["recomendación"]["negative"];
+           }
           
           
           
-        for (let keyword in (this.auditAgents[i].keywords)) {
-            
-            // console.log('keyword  = ',(keyword));
-            // console.log('clasification  = ',this.auditAgents[i].keywords[keyword].clasification.category);
-
-            let category=this.auditAgents[i].keywords[keyword].clasification.category;
-            if(!(category in agentCategoryTotals[nameAgent])){
-                (agentCategoryTotals[nameAgent])[category]={};
-            }
-            let haveResults=false;
-            if(this.auditAgents[i].keywords[keyword].results.length>0){
-                haveResults =true;
-
-            }
-
-            if(haveResults){
-                if(!("positive" in (agentCategoryTotals[nameAgent])[category])){
-                   (agentCategoryTotals[nameAgent][category])["positive"]=1;
-                }else{
-                    (agentCategoryTotals[nameAgent][category])["positive"]+=1;
-                }
-            }else{
-                 if(!("negative" in (agentCategoryTotals[nameAgent])[category])){
-                    (agentCategoryTotals[nameAgent][category])["negative"]=1;
-                }else{
-                    (agentCategoryTotals[nameAgent][category])["negative"]+=1;
-                }
-            }
-            
-            
-            
         }
         
-          
       }
-      console.log('agentCategoryTotals',agentCategoryTotals);
-      let agentCategoryArray=[];
-      for(let key in agentCategoryTotals){
-          let infoByAgent = {
-              name:key,
-              results:agentCategoryTotals[key]
-              
-          };
+      let topByCategoryArray=[];
+      for (let agent in topByCategory) {
+        let infoByAgent = {
+          name: agent,
+          results: topByCategory[agent]
+        };
 
-          agentCategoryArray.push(infoByAgent);
+        topByCategoryArray.push(infoByAgent);
       }
-      console.log('ver data dicc', agentCategoryArray)
-      this.agentCategoryTotals=agentCategoryArray;
-     // this.calls=agentCategoryTotals
-      //console.log('ver el total de la category', this.agentCategoryTotals)
+      
 
+      this.topByCategory = topByCategoryArray;
+      console.log("topByCategory", this.topByCategory);
+      
     }
   },
   computed: {
